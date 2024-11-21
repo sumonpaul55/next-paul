5243ff044a1dcba908d7faa9b36b15a6ff34ba3b
sumonpaul55 <sumpaul100@gmail.com>
2024-11-21 20:01:43 +0600

    resume for download change the name and it self

--- src/Cpages/home/Hero.tsx
+++ src/Cpages/home/Hero.tsx
@@ -23,7 +23,7 @@ export const Hero = () => {
                         <MyLink url='https://drive.google.com/file/d/11l4dOaHbE7IgGLos7ppB7y3ExshaKYf1/view?usp=sharing' target="_blank" className="bg-primary text-white border p-2 rounded-md font-semibold font-sans hover:bg-white hover:text-primary hover:border border-primary duration-400">
                             View Resume
                         </MyLink>
-                        <a href={`mern-end.pdf`} download target="_blank" className={`relative inline-block px-2 md:px-6 py-2 font-semibold text-primary border-1 border-primary rounded-lg overflow-hidden transition-colors duration-300 ease-in-out group hover:text-white`}>
+                        <a href={`resume.pdf`} download target="_blank" className={`relative inline-block px-2 md:px-6 py-2 font-semibold text-primary border-1 border-primary rounded-lg overflow-hidden transition-colors duration-300 ease-in-out group hover:text-white`}>
                             <span className="absolute inset-0 bg-primary transform translate-x-full transition-transform duration-500 ease-out group-hover:translate-x-0"></span>
                             <span className="relative z-10">Downdload Resume</span>
                         </a>
