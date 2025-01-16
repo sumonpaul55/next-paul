98980dc438f3ee8457d54516a07a163841a9e653
sumonpaul55 <sumpaul100@gmail.com>
2025-01-17 02:59:17 +0600

    project section

--- src/components/shared/ProjectsCard.tsx
+++ src/components/shared/ProjectsCard.tsx
@@ -10,19 +10,18 @@ const ProjectCard = ({ _id, liveLink, name, technology, image }: { _id: string; 
                 className="relative w-full h-[300px] shadow-lg overflow-hidden bg-cover bg-top hover:bg-bottom duration-1000"
                 style={{ backgroundImage: `url(${image})` }}// whileHover={{ scale: 1.05 }}
             >
-
-                <Link href={`/projects/${_id}`} className='bg-primary bg-opacity-80 p-2 w-[50%] absolute text-white -left-full group-hover:left-0 duration-500 bottom-0 hover:bg-blue-700'>
+                <Link href={`/projects/${_id}`} className='bg-primary bg-opacity-90 p-2 w-[50%] absolute text-white -left-full group-hover:left-0 duration-500 bottom-0 hover:bg-blue-700 text-sm'>
                     Project Details
                 </Link>
-                <Link href={liveLink} target='_blank' className='bg-primary bg-opacity-80 p-2 w-[50%] absolute text-white  -top-full group-hover:top-[87%] duration-200 left-[50.5%] hover:bg-blue-700'>
+                <Link href={liveLink} target='_blank' className='bg-primary bg-opacity-90 p-2 w-[50%] absolute text-white  -top-full group-hover:top-[88%] duration-200 left-[50.5%] hover:bg-blue-700 text-sm'>
                     View Live
                 </Link>
             </motion.div>
 
             <div className="inset-0 bg-black bg-opacity-30 flex items-end justify-between z-50">
                 {/* Project Info */}
-                <div className="text-left bg-gray-800 w-full px-3">
-                    <h2 className="text-white font-bold text-xl line-clamp-1">{name}</h2>
+                <div className="text-left bg-gray-800 w-full px-3 pt-3">
+                    <h2 className="text-white font-bold line-clamp-1">{name}</h2>
                     <p className="text-gray-300">{technology}</p>
                 </div>
                 {/* Project Overview - Initially Hidden */}
