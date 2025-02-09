1393a2afd0a17ad3bd448c162d73958af194c67c
sumonpaul55 <sumpaul100@gmail.com>
2025-02-09 19:53:25 +0600

    service section

--- src/Cpages/home/ServiceSection.tsx
+++ src/Cpages/home/ServiceSection.tsx
@@ -32,7 +32,7 @@ const services = [
 
 const ServiceSection = () => {
     return (
-        <section className="bg-black py-12">
+        <section className="bg-black py-12 px-5">
             <div className="px-6">
                 <h2 className="text-3xl font-bold text-white text-center mb-12">
                     My Services
